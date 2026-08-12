fd5ccd7d5c2e86ef30364a00918ec717d35e0fc7
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-08-12 19:30:23 -0300

    build(chore): Estruturação inicial das pastas e inicicalização do SpringBoot

---
+++ api/src/test/java/com/ong/acolhepatinhas/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ong.acolhepatinhas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
