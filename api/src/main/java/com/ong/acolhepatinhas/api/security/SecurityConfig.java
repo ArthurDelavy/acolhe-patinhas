1fc47942d506db9bbb8fc0a99d74ca7664b63e90
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-08-13 01:44:14 -0300

    feat(security): criacao da camada de  segurança baseada em jwt

---
+++ api/src/main/java/com/ong/acolhepatinhas/api/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.ong.acolhepatinhas.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(req -> {
+                req.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
+                req.requestMatchers("/error").permitAll();
+                req.anyRequest().permitAll();
+            })
+            // .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+}
