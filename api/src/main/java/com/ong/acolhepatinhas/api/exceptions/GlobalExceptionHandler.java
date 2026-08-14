3acf554f2eb75aa52f39245c1f28497dcc3992f8
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-08-14 15:13:03 -0300

    feat(exceptions): alteracao para especificar e tratar  lista de parametros incorretos no dto

--- api/src/main/java/com/ong/acolhepatinhas/api/exceptions/GlobalExceptionHandler.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.ong.acolhepatinhas.api.exceptions;
 
 import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,10 +19,17 @@ public class GlobalExceptionHandler {
     // Valor inválido nos DTOs
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException e) {
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
         ErrorResponse error = new ErrorResponse(
             HttpStatus.BAD_REQUEST.value(),
-            e.getMessage(),
-            OffsetDateTime.now()
+            "Um ou mais campos estão com valores inválidos",
+            OffsetDateTime.now(),
+            errors
         );
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
