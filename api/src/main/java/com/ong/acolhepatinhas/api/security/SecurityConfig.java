b248ea2fdce8dde00f7046e6fed4a75ab7ec3730
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-08-14 16:24:13 -0300

    fix(security): alteracao do anyrequest de permitall para authenticated

--- api/src/main/java/com/ong/acolhepatinhas/api/security/SecurityConfig.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(req -> {
                 req.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
                 req.requestMatchers("/error").permitAll();
-                req.anyRequest().permitAll();
+                req.anyRequest().authenticated();
             })
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
