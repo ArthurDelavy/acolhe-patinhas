4cc14ed5122e6a48a3b3761f1b361a382e6f4b2c
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-08-31 18:53:49 -0300

    feat(exception): criada excecao de imagens

--- api/src/main/java/com/ong/acolhepatinhas/api/exceptions/GlobalExceptionHandler.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/exceptions/GlobalExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.ong.acolhepatinhas.api.exceptions.custom.DuplicatedValueException;
 import com.ong.acolhepatinhas.api.exceptions.custom.ExpiredDataException;
+import com.ong.acolhepatinhas.api.exceptions.custom.ImageProcessingException;
 import com.ong.acolhepatinhas.api.exceptions.custom.ResourceInUseException;
 import com.ong.acolhepatinhas.api.exceptions.custom.ValueNotFoundException;
 
@@ -135,5 +136,16 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(ImageProcessingException.class)
+    public ResponseEntity<ErrorResponse> handleImageProcessing(ImageProcessingException e) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+            e.getMessage(), 
+            OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
---
+++ api/src/main/java/com/ong/acolhepatinhas/api/exceptions/custom/ImageProcessingException.java
@@ -0,0 +1,7 @@
+package com.ong.acolhepatinhas.api.exceptions.custom;
+
+public class ImageProcessingException extends RuntimeException {
+    public ImageProcessingException(String message) {
+        super(message);
+    }
+}
