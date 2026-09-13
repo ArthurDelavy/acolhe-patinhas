4f4e5395de52cd822a6746c294f7d76602076251
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-09-13 20:40:26 -0300

    feat(veterinary): adicionados limites de caracteres em dtos de criacao de doencas e vacinas

--- api/src/main/java/com/ong/acolhepatinhas/api/veterinary/disease/catalog/DTO/NewDiseaseRequest.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/veterinary/disease/catalog/DTO/NewDiseaseRequest.java
@@ -2,10 +2,11 @@ package com.ong.acolhepatinhas.api.veterinary.disease.catalog.DTO;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record NewDiseaseRequest(
 
-    @NotBlank
+    @NotBlank @Size(min = 3, max = 50)
     @Schema(example = "Dermatite")
     String name
 ) {
--- api/src/main/java/com/ong/acolhepatinhas/api/veterinary/vaccine/catalog/DTO/NewVaccineRequest.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/veterinary/vaccine/catalog/DTO/NewVaccineRequest.java
@@ -2,10 +2,11 @@ package com.ong.acolhepatinhas.api.veterinary.vaccine.catalog.DTO;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record NewVaccineRequest(
 
-    @NotBlank
+    @NotBlank @Size(min = 3, max = 30)
     @Schema(example = "Polivalente")
     String name
 ) {
