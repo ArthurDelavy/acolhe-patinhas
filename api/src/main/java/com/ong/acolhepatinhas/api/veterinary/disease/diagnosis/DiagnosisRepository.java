904bd268d3b9bf27c6c3ef1ae89642cd943442a6
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-09-13 17:31:26 -0300

    feat(disease): adicionada notacao de entitygraph no repository

--- api/src/main/java/com/ong/acolhepatinhas/api/veterinary/disease/diagnosis/DiagnosisRepository.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/veterinary/disease/diagnosis/DiagnosisRepository.java
@@ -2,6 +2,7 @@ package com.ong.acolhepatinhas.api.veterinary.disease.diagnosis;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,12 @@ import com.ong.acolhepatinhas.api.veterinary.disease.catalog.Disease;
 public interface DiagnosisRepository extends JpaRepository<Diagnosis, Integer>{
     boolean existsByDisease(Disease disease);
 
+    @EntityGraph(attributePaths = {"disease", "vetRecord.animal"})
+    List<Diagnosis> findAll();
+
+    @EntityGraph(attributePaths = {"disease", "vetRecord.animal"})
+    List<Diagnosis> findById(int id);
+
+    @EntityGraph(attributePaths = {"disease", "vetRecord.animal"})
     List<Diagnosis> findAllByVetRecord_Animal_Id(int animalId);
 }
