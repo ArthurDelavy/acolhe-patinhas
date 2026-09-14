4c978d967fb6368119dd3886d27f99bb5324af0e
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-09-13 21:01:48 -0300

    feat(disease): adicionado caching

--- api/src/main/java/com/ong/acolhepatinhas/api/veterinary/disease/catalog/DiseaseService.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/veterinary/disease/catalog/DiseaseService.java
@@ -2,6 +2,9 @@ package com.ong.acolhepatinhas.api.veterinary.disease.catalog;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -26,16 +29,19 @@ public class DiseaseService {
     private final DiagnosisService dgnSvc;
 
 
+    @Cacheable(value = "diseases")
     public List<Disease> listAll() {
         return dseRep.findAll();
     }
 
+    @Cacheable(value = "disease", key = "#diseaseId")
     public Disease getById(int diseaseId) {
         return dseRep.findById(diseaseId).orElseThrow(() -> new ValueNotFoundException("Doença não encontrada."));
     }
 
 
     @Transactional 
+    @CacheEvict(value = "diseases", allEntries = true)
     public Disease newDisease(@Valid NewDiseaseRequest data) {
 
         if (dseRep.existsByName(data.name())) throw new DuplicatedValueException("Doença já cadastrada.");
@@ -49,6 +55,10 @@ public class DiseaseService {
 
 
     @Transactional
+    @Caching(evict = {
+        @CacheEvict(value = "diseases", allEntries = true),
+        @CacheEvict(value = "disease", key = "#diseaseId")
+    })
     public void deleteDisease(int diseaseId) {
         Disease disease = this.getById(diseaseId);
         if (dgnSvc.existsByDisease(disease)) throw new ResourceInUseException("A doença não pôde ser deletada pois está vinculada a um diagnóstico.");
