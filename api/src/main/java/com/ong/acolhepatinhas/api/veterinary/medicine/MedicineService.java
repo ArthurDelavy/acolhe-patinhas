d5b826e58cb9a9d8943f1dac84550bb4079694a8
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-09-20 18:14:52 -0300

    fix(medicine): corrigida referencia circular de medicineService

--- api/src/main/java/com/ong/acolhepatinhas/api/veterinary/medicine/MedicineService.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/veterinary/medicine/MedicineService.java
@@ -2,9 +2,11 @@ package com.ong.acolhepatinhas.api.veterinary.medicine;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -27,7 +29,8 @@ public class MedicineService {
     
     private final MedicineRepository mdcRep;
 
-    private final TreatmentMedicineService tmcSvc;
+    @Autowired @Lazy
+    private TreatmentMedicineService tmcSvc;
     private final PreventiveCareService pvcSvc;
 
 
