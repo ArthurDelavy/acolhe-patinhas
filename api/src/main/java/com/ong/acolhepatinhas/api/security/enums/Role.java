8f22d17d8acb1e7a1d44cd07a9b8d848bdc70ee2
Arthur Delavy <arthurdelavy2015@gmail.com>
2026-09-25 22:46:28 -0300

    feat(security): adicionada role de verificado

--- api/src/main/java/com/ong/acolhepatinhas/api/security/enums/Role.java
+++ api/src/main/java/com/ong/acolhepatinhas/api/security/enums/Role.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public enum Role {
     ADMIN(Set.of(
+        Permission.USER_VERIFY,
         Permission.ANIMAL_READ,
         Permission.ANIMAL_CREATE,
         Permission.ANIMAL_EDIT,
@@ -20,6 +21,14 @@ public enum Role {
 
     USER(Set.of(
         Permission.ANIMAL_READ
+    )),
+    
+    VERIFIED(Set.of(
+        Permission.ANIMAL_READ,
+        Permission.ANIMAL_CREATE,
+        Permission.ANIMAL_EDIT,
+        Permission.VETERINARY_READ,
+        Permission.VETERINARY_ASSIGN
     ));
 
 
